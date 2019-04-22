4f407ad8a86857b9efc51d1382a2542472c309d8
ivankcmo <48834134+ivankcmo@users.noreply.github.com>
2019-04-22 14:42:14 -0500

    Day12Review

---
+++ src/Day12Review/MyHouse.java
@@ -0,0 +1,34 @@
+package Day12Review;
+
+
+public class MyHouse {
+ public int walls;
+    public int doors;
+    public int hallways;
+    public int windows;
+    public boolean roof;
+    public boolean floor;
+    public int levels;
+    public String foundation;
+    public boolean secretPassage;
+    public boolean hasSpirits;
+    public int fireplace;
+    public int raccoons;
+    public int deadbodies;
+    public int wheels;
+    public String engine;
+    public int mailbox;
+    public String mailboxcolor;
+
+
+
+
+    // What my Object does
+    public void openDoor(){
+        System.out.println("opened door");
+    }
+    public void closeDoor(){
+       System.out.println("Closed door");
+    }
+
+}
