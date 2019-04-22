0c5014ffa5a44a32a9fd93ffe3cf348442d43be8
ivankcmo <48834134+ivankcmo@users.noreply.github.com>
2019-04-22 09:37:55 -0500

    Look Ramone I got the DogYearCalc to work

---
+++ src/DogYearCalc1/DogClass.java
@@ -0,0 +1,36 @@
+package DogYearCalc1;
+
+public class DogClass {
+    public static String DogClassCalc(int age, int weight){
+        switch(""){
+            default:{
+                if(age < 2 && weight < 21){
+                    return "small boi";
+                }
+                else if(age > 1 && weight < 21){
+                    return "small boi";
+                }
+                else if(age > 1 && weight < 51){
+                    return "medium boi";
+                }
+                else if(age > 1 && weight < 200){
+                    return "Big boi";
+                }
+                else if(age < 2 && weight < 51){
+                    return "small chunky boi";
+                }
+                else if(age < 2 && weight > 50){
+                    return "bear cub";
+                }
+                else if(weight < 500){
+                    return "BEAR!, ok that is definitely a bear I don't know what you're talking about";
+                }
+                else if(weight > 499){
+                    return "wtf";
+                }
+
+            }
+        }
+        return null;
+    }
+}
